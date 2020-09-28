487a57561c7438f351ea10a92d3fb6d4b243503c
GeRongcun <gerongcun@gmail.com>
2020-09-28 22:12:17 +0800

    Update 教程.md

--- PythonCrawler/20200928爬取南京大学实验室安全考试题库_01单个题目.py
+++ PythonCrawler/20200928爬取南京大学实验室安全考试题库_01单个题目.py
@@ -2,9 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 import os # 用于文件目录操作
 
+# 教程：https://blog.csdn.net/hzwyjxy/article/details/85164067
+
 # url = "http://aqks.nju.edu.cn/pc/PersonInfo/StartExercise_Mobile.aspx?TestNum=1&SelTestNum=170&SelectTest=yes"
 url = "http://aqks.nju.edu.cn/pc/PersonInfo/StartExercise_Mobile.aspx"
+# TestNum不知道是什么含义
+# SelTestNum表示题目序号
 d={'TestNum':'1','SelTestNum':'1','SelectTest':'yes'}
+
+# 头部信息
 headers = {
     # 用户标识
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36 Edg/85.0.564.63',
@@ -14,36 +20,50 @@ headers = {
     'content-type': 'application/x-www-form-urlencoded'
 }
 
+# post获取网页数据
 response=requests.post(url,data=d,headers=headers)
+# 打印网页数据(HTML文本)
 print(response.text)
-
+# BeautifulSoup是HTML解析器，用于提取信息
+# 将html文本转换为文档对象
 soup=BeautifulSoup(response.text)
+# 打印文档对象
 soup
-QuestionTable=soup.find_all('table',{'id':'trTestTypeContent1040'})
+# 提取出id为'trTestTypeContent1'(题目的id='trTestTypeContent'+题目序号)的table标签
+QuestionTable=soup.find_all('table',{'id':'trTestTypeContent1'})
+# 打印，查看信息
 QuestionTable
 
-str=""
-
-# 题目、答案和解析
-test=QuestionTable[0].find_all('td')
-# for i in range(len(test)):
-for i in  test:
-    if(i.text!=''):
-        # print(i.text)
-        str+=i.text+'\n'
-str
+# 最终成果文本
+resultStr=""
 
 # 题目类型 单选题/判断题/多选题
+# 找到input标签
 str1=QuestionTable[0].find('input')
 str1
 str1["value"]
-str=str+str1["value"]+'\n'
-str
+# input标签的value属性值记录了题目类型
+resultStr=resultStr+str1["value"]+'\n'
+resultStr
 
+# 题目、答案和解析
+# 找到所有td标签
+QuestionStr=QuestionTable[0].find_all('td')
+# for i in range(len(test)):
+for i in QuestionStr:
+    # 如果不是空行，就追加该行
+    if(i.text!=''):
+        # print(i.text)
+        resultStr+=i.text+'\n'
+# 打印，查看信息
+resultStr
+
+# 打开文件夹
 dataFolder=r"D:\00\20200928爬取南京大学实验室安全考试题库"
 os.chdir(dataFolder) # 打开文件目录，相当于cd
 os.getcwd() # 返回当前工作目录，显示当前文件目录
 
+# 将成果文本写入文本文件
 a= open('a.txt','w')
-a.write(str)
+a.write(resultStr)
 a.close()
--- PythonCrawler/20200928爬取南京大学实验室安全考试题库_02多个题目for循环.py
+++ PythonCrawler/20200928爬取南京大学实验室安全考试题库_02多个题目for循环.py
@@ -1,17 +1,8 @@
-'''
-@Author: your name
-@Date: 2020-03-16 11:12:27
-@LastEditTime: 2020-03-18 21:29:01
-@LastEditors: Please set LastEditors
-@Description: In User Settings Edit
-'''
 import requests
-# # 借助BeautifulSoup包解析
-from bs4 import BeautifulSoup
-# import re #使用正则表达式库,可以用这个库实现字符串片段匹配
+from bs4 import BeautifulSoup # 借助BeautifulSoup包解析
 import os # 用于文件目录操作
-# import shutil # 用于文件目录的高级操作
 
+# 头部
 headers = {
     # 'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.0',
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36 Edg/85.0.564.63',
@@ -20,23 +11,31 @@ headers = {
     # 'content-type': 'content-type: text/html; charset=UTF-8'
 }
 
+# 最终成果文本
 resultStr=""
+
+# 从第1题循环到第1040题
 for i in range(1, 1041):
     print(i)
     # url = "http://aqks.nju.edu.cn/pc/PersonInfo/StartExercise_Mobile.aspx?TestNum=1&SelTestNum=170&SelectTest=yes"
+    # TestNum含义不明，值为1，好像是题目种类，好像又不是
+    # SelTestNum表示题号
     url = "http://aqks.nju.edu.cn/pc/PersonInfo/StartExercise_Mobile.aspx"
-    # TestNum表示类别
-    d={'TestNum':'3','SelTestNum':'','SelectTest':'yes'}
+    d={'TestNum':'1','SelTestNum':'','SelectTest':'yes'}
+    # 修改题号
     d['SelTestNum']=str(i)
+    # post获取网页
     response=requests.post(url,data=d,headers=headers)
+    # 解析网页
     soup=BeautifulSoup(response.text)
-    # soup
+    # 构造id
     idStr='trTestTypeContent'+str(i)
-    # QuestionTable=soup.find_all('table',{'id':'trTestTypeContent1040'})
+    # 找到id为idStr的table标签
+    # QuestionTable=soup.find_all('table',{'id':'trTestTypeContent1'})
     QuestionTable=soup.find_all('table',{'id':idStr})
+    # 打印，查看信息
     # QuestionTable
 
-    
     # 题目类型 单选题/判断题/多选题
     resultStr=resultStr+'题目类型：'+QuestionTable[0].find('input')["value"]+'\n'
 
@@ -49,14 +48,15 @@ for i in range(1, 1041):
             resultStr+=i.text+'\n'
     resultStr+='\n'
 
-
+# 查看成果文本
 resultStr
 
-
+# 打开文件夹
 dataFolder=r"D:\00\20200928爬取南京大学实验室安全考试题库"
 os.chdir(dataFolder) # 打开文件目录，相当于cd
 os.getcwd() # 返回当前工作目录，显示当前文件目录
 
+# 写入文本文件
 a= open('a.txt','w',encoding='UTF-8')
 a.write(resultStr)
 a.close()
